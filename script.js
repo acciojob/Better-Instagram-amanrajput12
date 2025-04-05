e3b5a8ac885678080e599dbf171d2e0d27168cda
Aman Rajput <89723728+amanrajput12@users.noreply.github.com>
2025-04-05 17:19:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,55 @@
 //your code here
+// Assign IDs to each image div
+document.querySelectorAll(".image").forEach((value, i) => {
+    value.setAttribute("id", `div${i + 1}`);
+});
+
+let draggedElement = null;
+
+// Add drag events
+document.querySelectorAll(".image").forEach(image => {
+    // Drag start
+    image.addEventListener("dragstart", (e) => {
+        draggedElement = e.target;
+        // e.target.classList.add("dragging");
+    });
+
+    // Drag end
+    image.addEventListener("dragend", (e) => {
+        // e.target.classList.remove("dragging");
+    });
+
+    // Drag over
+    image.addEventListener("dragover", (e) => {
+        e.preventDefault(); // Necessary to allow drop
+    });
+
+    // Drop
+    image.addEventListener("drop", (e) => {
+        e.preventDefault();
+
+        if (draggedElement === e.target) return;
+
+        const draggedBg = getComputedStyle(draggedElement).backgroundImage;
+        const draggedvalue=draggedElement.innerHTML;
+        const draggedId =draggedElement.id;
+
+        const targetBg = getComputedStyle(e.target).backgroundImage;
+        const targetvalue=e.target.innerHTML;
+        const targetId =e.target.id;
+
+        // console.log("on drag ",draggedBg,draggedvalue,draggedId)
+        // console.log("target value ",targetBg,targetvalue,targetId)
+      
+       
+        draggedElement.style.backgroundImage =targetBg;
+        draggedElement.innerHTML=targetvalue;
+     
+        draggedElement.id=targetId
+
+        e.target.style.backgroundImage =draggedBg;
+        e.target.innerHTML=draggedvalue;
+     
+        e.target.id=draggedId;
+    });
+});
